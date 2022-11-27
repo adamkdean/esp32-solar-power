1d1761513b908a20cbc440f40327758289cd3cbe
Adam K Dean <adamkdean@googlemail.com>
2022-11-27 22:16:29 +0000

    add arduino stub (stupid)

---
+++ firmware/firmware.ino
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 Adam K Dean <adamkdean@googlemail.com>
+// Use of this source code is governed by the GPL-3.0
+// license that can be found in the LICENSE file.
+
+// Intentionally blank file due to ridiculous Arduino requirements:
+// See https://github.com/arduino/arduino-cli/issues/948
+// And https://github.com/arduino/arduino-cli/pull/690/files#diff-76bc1d481fc32adae0d5a4ee3b2ccdf0R46
