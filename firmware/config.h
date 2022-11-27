eb525781df739051c8c89f6da7060039794c04ec
Adam K Dean <adamkdean@googlemail.com>
2022-11-27 22:16:38 +0000

    add config file

---
+++ firmware/config.h
@@ -0,0 +1,6 @@
+// Copyright (C) 2022 Adam K Dean <adamkdean@googlemail.com>
+// Use of this source code is governed by the GPL-3.0
+// license that can be found in the LICENSE file.
+
+#define WIFI_SSID "SSID_HERE"
+#define WIFI_PASSWORD "PASSWORD_HERE"
